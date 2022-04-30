b5e84826ea135d8d0e8637ec57ae42b6c4c8ed6f
Harry Law <53987565+h5law@users.noreply.github.com>
2022-04-30 11:54:24 +0100

    Make store persistent

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
 
 import budgetsSlice from '../features/budgets/budgetsSlice.js';
 import transactionsSlice from '../features/transactions/transactionsSlice.js';
 
+const reducers = combineReducers({
+  budgets: budgetsSlice,
+  transactions: transactionsSlice,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = configureStore({
-  reducer: {
-    budgets: budgetsSlice,
-    transactions: transactionsSlice,
-  }
+  reducer: persistedReducer,
+  devTools: process.env.NODE_END !== 'production',
+  middleware: [thunk],
 });
 
 export default store;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
 import './index.css';
 import App from './app/App';
 import store from './app/store.js';
 
+let persistor = persistStore(store);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
